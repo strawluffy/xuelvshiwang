56e249b73ece4bb06823363488f9f4ebcb30d4f2
strawluffy <cui_zz@126.com>
2014-11-18 15:31:35 +0800

    增加百度地图

--- login/views.py
+++ login/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render_to_response
 def hello(request):
 	name='hello'
 	return render_to_response('login/base.html',{'name':name})
+def index(request):
+	return render_to_response('login/index.html')
--- xuelvshiwang/urls.py
+++ xuelvshiwang/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', 'login.views.hello'),
+    url(r'^$', 'login.views.index'),
 )
