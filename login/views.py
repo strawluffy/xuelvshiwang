8e8b80677d53bac669bd67383b8480c047c03ccd
strawluffy <cui_zz@126.com>
2014-11-19 07:39:20 +0800

    修正倒计时时间，使用服务器时间而不是本地时间

--- login/views.py
+++ login/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render_to_response
-
+from django.utils import timezone
+import datetime,time
 def hello(request):
 	name='hello'
-	return render_to_response('login/base.html',{'name':name})
+	local_time=timezone.now()
+	return render_to_response('login/base.html',{'name':name,'local_time':local_time})
 def index(request):
-	return render_to_response('login/index.html')
+	endtime=time.mktime(datetime.datetime(2014,12,01,0,0).timetuple())
+	now=time.time()
+	delta=int(endtime-now)
+	return render_to_response('login/index.html',{'delta':delta})
